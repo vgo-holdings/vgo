d79dee9955f819af951a35d2a030c5ba55731dbd
vgo-holdings <151865771+vgo-holdings@users.noreply.github.com>
2023-11-28 07:37:48 +0530

    Update page.js

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -1,126 +1,130 @@
 "use client";
 
-import { useState } from 'react'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
-import './page-style.css';
+import { useRouter } from "next/navigation";
+import ComponentLevelLoader from "../Loader/componentlevel";
+import './style.css'
+export default function CommonCart({
+  cartItems = [],
+  handleDeleteCartItem,
+  componentLevelLoader,
+}) {
 
-const ContactForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // You can implement form submission logic here, e.g., send data to an API or handle it in your Next.js backend.
-    };
-    return (
-        <form onSubmit={handleSubmit}>
-      <div className="mb-4">
-        <label htmlFor="name" className="block text-lg font-semibold mb-2">Your Name</label>
-        <input type="text" id="name" className="w-full p-2 border rounded" value={name} onChange={(e) => setName(e.target.value)} required />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-lg font-semibold mb-2">Email Address</label>
-        <input type="email" id="email" className="w-full p-2 border rounded" value={email} onChange={(e) => setEmail(e.target.value)} required />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="message" className="block text-lg font-semibold mb-2">Message</label>
-        <textarea id="message" className="w-full p-2 border rounded" value={message} onChange={(e) => setMessage(e.target.value)} required />
-      </div>
-      <button type="submit" className="disabled:opacity-50 disabled:cursor-not-allowed inline-flex sm:w-full contact-mailBtn items-center justify-center px-6 py-4 text-lg 
-                     text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase tracking-wide rounded-lg w-1/5 border mr-10 border-transparent shadow-sm hover:bg-white hover:text-white " style={{backgroundColor: "#e84118"}}>Send Message</button>
-    </form>
-    );
-  };
-
-  const GoogleMapComponent = ({ center, zoom }) => {
-    const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
-
-    return (
-      <LoadScript googleMapsApiKey={googleMapsApiKey}>
-        <GoogleMap center={center} zoom={zoom} mapContainerStyle={{ width: '100%', height: '400px' }}>
-          <Marker position={center} />
-        </GoogleMap>
-      </LoadScript>
-    );
-  };
-  
-
-export default function ContactUs() {
-    const mapCenter = {lat :7.3018 , lng:81.6747 };
-    const mapZoom = 13;
+  const router = useRouter()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between   md:p-12 text-black">
-        <div className=" w-full"> 
-
-        <div className='contact-detailsContainer'>
-          <div className='contact-dataContainer'>
-            <div className='contact-mapContainer relative'>  
-            <h1
-                className=" font-bold  text-4xl mb-8 "
-                style={{zIndex:'5', color: "#2F3640",  fontSize:'2.5rem',textAlign:'left'}}
-              >
-                Contact <span style={{ color: "#e84118" }}>Details </span>
-              </h1>
-                <GoogleMapComponent center={mapCenter} zoom={mapZoom} /> 
-                <div className='contact-contactDataContainer'>
-                <div div className="container mx-auto">
-              
-                <div className="contact-contactDataGride">
-                  <div className=' mb-4'>
-                    <h3 className="text-lg font-semibold">Address</h3>
-                    <p style={{ color: "#e84118" }}>Your Company Address</p>
-                  </div>
-                  <div className='mb-4 '>
-                    <h3 className="text-lg font-semibold">Email</h3>
-                    <p style={{ color: "#e84118" }}>vigouerholdings@gmail.com</p>
-                  </div>
-                  <div className=' mb-4'>
-                    <h3 className="text-lg font-semibold">Phone</h3>
-                    <p style={{ color: "#e84118" }}>+94777034524</p>
-                  </div>
-                  <div className=' mb-4'>
-                    <h3 className="text-lg font-semibold">Hours</h3>
-                    <p style={{ color: "#e84118" }}>Mon-Fri: 9:00 AM - 5:00 PM</p>
-                  </div>
+    <section className="h-screen bg-gray-100 pt-10"> 
+          <div className=" cart-container"> 
+              <div className="cart-leftContainer relative">
+              <h1 className=" font-bold   cart-header"
+                style={{zIndex:'5', color: "#2F3640",  fontSize:'2.5rem',textAlign:'left'}}>Shopping <span style={{ color: "#e84118" }}>Cart </span></h1>
+                {cartItems && cartItems.length ? (
+                  <ul className="cart-cartList">
+                    {cartItems.map((cartItem) => (
+                      <li
+                        className="cart-cartItem"
+                        key={cartItem.id}
+                        style={{backgroundColor:'rgba(250, 250, 250, 0.555)',backdropFilter:'blur(5px)',borderRadius:'8px'}}
+                      >
+                        <div className="shrink-0">
+                          <img
+                            src={
+                              cartItem &&
+                              cartItem.productID &&
+                              cartItem.productID.imageUrl
+                            }
+                            alt="Product image"
+                            className="h-24 w-25 max-w-full rounded-lg object-cover"
+                          />
+                        </div>
+                        <div className="flex flex-1 flex-col justify-between">
+                          <div className="sm:col-gap-5 sm:grid sm:grid-cols-2">
+                            <div className="pr-8 sm:pr-4">
+                              <p className="text-base font-semibold text-gray-900">
+                                {cartItem &&
+                                  cartItem.productID &&
+                                  cartItem.productID.name}
+                              </p>
+                            </div>
+                            <div className="mt-4 flex gap-3 items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                              <p className="shrink-0 w-20 text-base font-semibold text-gray-950 sm:order-1 sm:ml-8 sm:text-right">
+                                LKR
+                                {cartItem &&
+                                  cartItem.productID &&
+                                  cartItem.productID.price}
+                              </p>
+                              <button
+                                type="button"
+                                className="font-medium text-yellow-700 sm:order-2"
+                                onClick={() =>
+                                  handleDeleteCartItem(cartItem._id)
+                                }
+                              >
+                                {componentLevelLoader &&
+                                componentLevelLoader.loading &&
+                                componentLevelLoader.id === cartItem._id ? (
+                                  <ComponentLevelLoader
+                                    text={"Removing"}
+                                    color={"#0000000"}
+                                    loading={
+                                      componentLevelLoader &&
+                                      componentLevelLoader.loading
+                                    }
+                                  />
+                                ) : (
+                                  "Remove"
+                                )}
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <h1 className="font-bold text-lg">Your cart is Empty !</h1>
+                )}
+              </div>
+              <div className=" cart-rightContainer">
+                <div className="flex mt-8  mb-2 w-full items-center pt-4 justify-between">
+                  <p className="text-2xl text-black ">Sub Total</p>
+                  <p className="text-2xl text-black font-semibold">
+                    $
+                    {cartItems && cartItems.length
+                      ? cartItems.reduce(
+                          (total, item) => item.productID.price + total,
+                          0
+                        )
+                      : "0"}
+                  </p>
                 </div>
-                
-              </div>
-          
-            </div>
-             </div>
-             <div className="container  contact-emailContainer" >
-                <h1 className=" font-bold  text-4xl mb-8 "
-                style={{zIndex:'5', color: "#2F3640",  fontSize:'2.5rem',textAlign:'left'}}>Send Us <span style={{ color: "#e84118" }}>a Message </span></h1>
-                <div className='p-2 md:p-8'>
-                <ContactForm />
+                <div className="flex mt-3 mb-8 w-full items-center justify-between">
+                  <p className="text-2xl text-black">Shipping Cost</p>
+                  <p className="text-2xl text-black font-semibold">$0</p>
                 </div>
-                <div className="contact-socialContainer">
-                  <a href="https://www.facebook.com/profile.php?id=61552658554263&mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer" className="  socialIcon">
-                     <i className='fab fa-facebook' style={{fontSize:'40px',color:'#e84118'}}></i>
-                  </a>
-                  <a href="https://www.instagram.com/vgoholdings/" target="_blank" rel="noopener noreferrer" className="  socialIcon">
-                     <i className='fab fa-instagram' style={{fontSize:'40px',color:'#e84118'}}></i>
-                  </a>
-                  <a href="https://www.youtube.com/channel/UCP0Xr5X-AbJnb0Kjk8B26vw" target="_blank" rel="noopener noreferrer" className="  socialIcon">
-                     <i className='fab fa-twitter' style={{fontSize:'40px',color:'#e84118'}}></i>
-                  </a>
-                  <a href="https://www.tiktok.com/@vgoonlinestore?_t=8gq3Qcer1ak&_r=1" target="_blank" rel="noopener noreferrer" className="  socialIcon">
-                    <i className='fab fa-tiktok' style={{fontSize:'40px',color:'#e84118'}}></i>
-                  </a>
-                  <a href="https://wa.me/+94777034524" target="_blank" rel="noopener noreferrer" className="  socialIcon">
-                    <i className='fab fa-whatsapp' style={{fontSize:'40px',color:'#e84118'}}></i>
-                  </a>
+                <hr className="divider"/>
+                <div className="flex mt-4 mb-8 w-full items-center justify-between">
+                  <p className="text-4xl font-bold text-black">Total</p>
+                  <p className="text-4xl text-black font-bold">
+                    $
+                    {cartItems && cartItems.length
+                      ? cartItems.reduce(
+                          (total, item) => item.productID.price + total,
+                          0
+                        )
+                      : "0"}
+                  </p>
+                </div>
+                <div className="mt-8 w-full text-center">
+                  <button
+                  onClick={()=>router.push('/checkout')}
+                    disabled={cartItems && cartItems.length === 0}
+                    className="disabled:opacity-50 group inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white font-medium uppercase tracking-wide"
+                  >
+                    Checkout
+                  </button>
+                </div>
               </div>
-              </div>
-              
-          </div>
-          <div>
-            
-          </div>
-        </div> 
-    </div>
-    </main>
+            </div> 
+    </section>
   );
 }
