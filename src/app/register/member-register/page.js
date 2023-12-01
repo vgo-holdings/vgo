743aeb47cf4e5add56d7ff220ad33a52173fbd76
vgo-holdings <151865771+vgo-holdings@users.noreply.github.com>
2023-12-01 15:59:06 +0530

    Update page.js

--- src/app/register/member-register/page.js
+++ src/app/register/member-register/page.js
@@ -351,6 +351,7 @@ export default function Register() {
                               [controlItem.id]: event.target.value,
                             });
                           }}
+                          disabled= {controlItem.disabled || false}
                           value={depositFormData[controlItem.id]}
                         />
                       ) : controlItem.componentType === "select" ? (
