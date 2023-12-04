8e6818aa1999f841dbb5877a62fc259d5229a306
vgo-holdings <151865771+vgo-holdings@users.noreply.github.com>
2023-12-04 11:50:29 +0530

    Update page.js

--- src/app/hr-view/seller-slip/page.js
+++ src/app/hr-view/seller-slip/page.js
@@ -1,9 +1,9 @@
 import DepositListing from "@/components/DepositListing";
-import { getAllSellerDeposits } from "@/services/bank-deposit";
+import { getAllDeposits } from "@/services/bank-deposit";
 
 export default async function AdminAllUsers() {
 
-  const allSellerDeposits = await getAllSellerDeposits()
+  const allSellerDeposits = await getAllDeposits();
 
   return <DepositListing data={allSellerDeposits && allSellerDeposits.data}/>
 }
