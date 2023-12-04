2a361d9f08e593b6b5c6382701d5eb5b0f07d260
vgo-holdings <151865771+vgo-holdings@users.noreply.github.com>
2023-12-04 14:55:21 +0530

    Update route.js

--- src/app/api/update-image/route.js
+++ src/app/api/update-image/route.js
@@ -5,6 +5,8 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function PUT(req1) {
+  let finalData;
+  
   try {
     await connectToDB();
     const extractData = await req1.json();
@@ -26,9 +28,36 @@ export async function PUT(req1) {
     console.log(updatedUser, "why this");
 
     if (updatedUser) {
+      if (updatedUser.class_name !== "") {
+        const extractAllClasses = await Class_data.find({ _id: updatedUser.class_name });
+        finalData = {
+          user: {
+            email: updatedUser.email,
+            name: updatedUser.name,
+            _id: updatedUser._id,
+            role: updatedUser.role,
+            imageURL: updatedUser.imageURL,
+            createdAt: updatedUser.createdAt,
+            class_name: extractAllClasses[0]?.name,
+          },
+        };
+      } else {
+        finalData = {
+          user: {
+            email: updatedUser.email,
+            name: updatedUser.name,
+            _id: updatedUser._id, 
+            role: updatedUser.role,
+            imageURL: updatedUser.imageURL,
+            createdAt: updatedUser.createdAt,
+          },
+        };
+      }
+
       return NextResponse.json({
         success: true,
         message: "Success",
+        finalData,
       });
     } else {
       return NextResponse.json({
