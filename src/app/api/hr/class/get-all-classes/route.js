21cbb2442bd6db1f8b44ac68e29782c84569c1f0
vgo-holdings <151865771+vgo-holdings@users.noreply.github.com>
2023-12-07 17:31:53 +0530

    Update route.js

--- src/app/api/hr/class/get-all-classes/route.js
+++ src/app/api/hr/class/get-all-classes/route.js
@@ -12,11 +12,37 @@ export async function GET(req) {
 
     if (isAuthUser?.role === "hr") {
       const getAllClasses = await Class_data.find({})
+      const classDataWithUsers = [];
+
+      for (let i = 0; i < getAllClasses.length; i++) {
+        const classId = getAllClasses[i]._id;
+        const users = await User.find({ class_name: classId }, { name: 1, class_lvl: 1 }).sort({'updatedAt':1});
+        console.log("🚀 ~ file: route.js:20 ~ GET ~ users:", users)
+        
+        const classWithUsers = {
+          class: {
+            _id: getAllClasses[i]._id,
+            name: getAllClasses[i].name,
+            lvl3_count: [],
+            lvl4_count: [],
+            lvl2_count: [],
+            lvl1_count: [],
+          },
+          users: users,
+        };
+
+        users.forEach((user) => {
+          classWithUsers.class[`lvl${user.class_lvl}_count`].push(user.name);
+        });
+
+        classDataWithUsers.push(classWithUsers);
+
+      }
 
       if (getAllClasses) {
         return NextResponse.json({
           success: true,
-          data: getAllClasses,
+          data: classDataWithUsers,
         });
       } else {
         return NextResponse.json({
