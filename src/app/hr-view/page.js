ce04519c6d57e62711b1a781d5200859c4f23f97
vgo-holdings <151865771+vgo-holdings@users.noreply.github.com>
2023-12-07 17:33:45 +0530

    Update page.js

--- src/app/hr-view/page.js
+++ src/app/hr-view/page.js
@@ -54,54 +54,63 @@ export default function AdminView() {
   return (
     <div className="mx-auto w-full mt-8 p-24">
       {classData.map((classItem, index) => (
-      <>  
-      <h1 className="text-center text-2xl font-bold mb-4">Table Id : {classItem._id}</h1>
-      <h1 className="text-center text-2xl font-bold mb-4">Table type : {classItem.name}</h1>
-      <table className="w-full table-auto border-collapse mb-20" key={index}>
-        <tbody className="text-center border border-black">
-          <tr className="border border-black h-20">
-            {[1].map((column) => (
-              <td colSpan="8" key={column} className="bg-yellow-500 py-2 h-full border border-black">
-                Column {column}
-              </td>
-            ))}
-          </tr>
-          <tr className="border border-black h-20">
-          {Array.from({ length: classItem?.lvl2_count || 0 }).map((_, columnIndex) => (
-              <td
-                key={columnIndex}
-                colSpan={4}
-                className="bg-red-500 py-2 h-full border border-black"
-              >
-                {classItem?.lvl2_count}
-              </td>
-            ))}
-          </tr>
-          <tr className="border border-black h-20">
-          {Array.from({ length: classItem?.lvl3_count || 0 }).map((_, columnIndex) => (
-              <td
-                key={columnIndex}
-                colSpan={2}
-                className="bg-purple-500 py-2 h-full border border-black"
-              >
-                {classItem?.lvl3_count}
-              </td>
-            ))}
-          </tr>
-          <tr className="border border-black h-20">
-          {Array.from({ length: classItem?.lvl4_count || 0 }).map((_, columnIndex) => (
-              <td
-                key={columnIndex}
-                colSpan={1}
-                className="bg-blue-500 py-2 h-full border border-black"
-              >
-                {classItem?.lvl4_count}
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
-      </>
+        <div key={index}>
+          <h1 className="text-center text-2xl font-bold mb-4">Table Id : {classItem.class._id}</h1>
+          <h1 className="text-center text-2xl font-bold mb-4">Table type : {classItem.class.name}</h1>
+          <table className="w-full table-auto border-collapse mb-20" key={index}>
+            <tbody className="text-center border border-black">
+              <tr className="border border-black h-20">
+                {Array.from({ length: classItem.class.lvl1_count.length || 0 }).map((_, columnIndex) => (
+                  <td
+                    key={columnIndex}
+                    colSpan={8}
+                    className="bg-green-500 py-2 h-full border border-black"
+                  >
+                    <ul>
+                      {classItem.class.lvl1_count.map((userName, userIndex) => (
+                        <li key={userIndex}>{userName}</li>
+                      ))}
+                    </ul>
+                  </td>
+                ))}
+              </tr>
+              <tr className="border border-black h-20">
+                {Array.from({ length: classItem.class.lvl2_count.length || 0 }).map((_, columnIndex) => (
+                  <td
+                    key={columnIndex}
+                    colSpan={4}
+                    className="bg-red-500 py-2 h-full border border-black"
+                  >
+                    {classItem.class.lvl2_count[columnIndex]}
+  
+                  </td>
+                ))}
+              </tr>
+              <tr className="border border-black h-20">
+                {Array.from({ length: classItem.class.lvl3_count.length || 0 }).map((_, columnIndex) => (
+                  <td
+                    key={columnIndex}
+                    colSpan={2}
+                    className="bg-purple-500 py-2 h-full border border-black"
+                  >
+                    {classItem.class.lvl3_count[columnIndex]}
+                  </td>
+                ))}
+              </tr>
+              <tr className="border border-black h-20">
+                {Array.from({ length: classItem.class.lvl4_count.length || 0 }).map((_, columnIndex) => (
+                  <td
+                    key={columnIndex}
+                    colSpan={1}
+                    className="bg-blue-500 py-2 h-full border border-black"
+                  >
+                    {classItem.class.lvl4_count[columnIndex]}
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
       ))}
     </div>
   );
