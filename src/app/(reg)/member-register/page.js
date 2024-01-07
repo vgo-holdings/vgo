cfa0fe7b8d9feb6d8589045f50291c4f71147f00
vgo-holdings <151865771+vgo-holdings@users.noreply.github.com>
2024-01-07 23:59:44 +0530

    fixed mem reg deposit part

--- src/app/(reg)/member-register/page.js
+++ src/app/(reg)/member-register/page.js
@@ -248,6 +248,7 @@ export default function Register() {
                     refkey: "",
                 })
             }
+            setSelectedImage("");
             setLoading(false);
         }
     }
