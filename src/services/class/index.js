d969217eecbfd19e68e03e6470b35d22b47ed999
vgo-holdings <151865771+vgo-holdings@users.noreply.github.com>
2024-01-09 01:50:36 +0530

    test user class view

--- src/services/class/index.js
+++ src/services/class/index.js
@@ -16,3 +16,39 @@ export const getAllClassDatas = async () => {
       console.log(e);
     }
   };
+
+export const getClassDataById = async (id) => {
+    try {
+      const res = await fetch(`/api/get-all-classes?id=${id}`, {
+        method: "GET",
+        cache: "no-store",
+        // headers: {
+        //   Authorization: `Bearer ${Cookies.get("token")}`,
+        // },
+      });
+  
+      const data = await res.json();
+  
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // export const shopById = async (id) => {
+  //   try {
+  //     const res = await fetch(
+  //       `https://www.vgo.lk/api/admin/shop-by-id?id=${id}`,
+  //       {
+  //         method: "GET",
+  //         cache: "no-store",
+  //       }
+  //     );
+  
+  //     const data = await res.json();
+  
+  //     return data;
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
